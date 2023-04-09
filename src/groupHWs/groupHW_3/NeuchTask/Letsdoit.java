74f1b24cf351c94813da20b7976c437d6f83f427
RomanIvanov-atom <atom10121998@mail.ru>
2023-04-09 14:51:51 +0300

    Add Neuch's task for groupHW_3 - 5

---
+++ src/groupHWs/groupHW_3/NeuchTask/Letsdoit.java
@@ -0,0 +1,28 @@
+package groupHWs.groupHW_3.NeuchTask;
+
+import static java.lang.Math.pow;
+
+public class Letsdoit {
+    public static String letsDoIt(int powerOf2){
+        StringBuffer str = new StringBuffer("a");
+        if (powerOf2 < 0){
+            return "Vvedite chislo bolshe 0 !";
+        } else if (powerOf2 == 0) {
+            return str.toString();
+        } else {
+            int lengthOut = (int) pow(2, powerOf2);
+
+            for(int i = 0; i<powerOf2; i++){
+                str.append(str); //a //ab //abaa // abaaabab ...
+
+                if ( str.charAt(str.length()-1) == 'a' ){
+                    str.setCharAt(str.length()-1, 'b');
+                } else {
+                    str.setCharAt(str.length()-1, 'a');
+                }
+            }
+            return "There are " + lengthOut + " symbols in output string: \n" + str.toString();
+        }
+    }
+
+}
