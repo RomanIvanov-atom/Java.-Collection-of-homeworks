b3faf7592175c04e6fd8ac27e5f4634d65f3ddfc
RomanIvanov-atom <atom10121998@mail.ru>
2023-04-09 13:08:18 +0300

    Add myTask for groupHW_3(task 'ReversedWords') - 3

---
+++ src/groupHWs/groupHW_3/MyTask_ReversedWords/Main.java
@@ -0,0 +1,7 @@
+package groupHWs.groupHW_3.MyTask_ReversedWords;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(ReverseWords.reverseWords("first second third fourth fifth sixth"));
+    }
+}
