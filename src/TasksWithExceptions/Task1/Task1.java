98466bcbbf5751ab4f62a128f64c75021b264da8
Roman Ivanov <atom10121998@mail.ru>
2023-05-30 14:14:10 +0300

    rename main -> Task1 in TasksWithExceptions folder - 8.1

--- src/TasksWithExceptions/Task1/Task1.java
+++ src/TasksWithExceptions/Task1/Task1.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
           выдать сообщение об этом
 */
 
-public class Main {
+public class Task1 {
     public static void main(String[] args) {
         System.out.println("Enter integer number: ");
 
